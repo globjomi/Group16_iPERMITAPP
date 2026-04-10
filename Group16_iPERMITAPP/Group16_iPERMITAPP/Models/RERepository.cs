5cefe5a0370be55b572156eb485fc6e8beec97da
Amwaj Sabri <amwajsabri@gmail.com>
2026-04-09 22:00:16 -0500

    added file

--- Group16_iPERMITAPP/Group16_iPERMITAPP/Models/RERepository.cs
+++ Group16_iPERMITAPP/Group16_iPERMITAPP/Models/RERepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SQLite;
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
 using Group16_iPERMITAPP.Database;
 
 namespace Group16_iPERMITAPP.Models
@@ -36,6 +37,37 @@ namespace Group16_iPERMITAPP.Models
             return null;
         }
 
+        // New overload: get RE by id only
+        public RE GetById(string id)
+        {
+            using (var conn = DbHelper.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT REId, REPassword, contactPersonName, createdDate, REEmail, organizationName, organizationAddress FROM RE WHERE REId = @id;";
+                using (var cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new RE
+                            {
+                                REId = reader.IsDBNull(0) ? null : reader.GetString(0),
+                                REPassword = reader.IsDBNull(1) ? null : reader.GetString(1),
+                                ContactPersonName = reader.IsDBNull(2) ? null : reader.GetString(2),
+                                CreatedDate = reader.IsDBNull(3) ? null : reader.GetString(3),
+                                REEmail = reader.IsDBNull(4) ? null : reader.GetString(4),
+                                OrganizationName = reader.IsDBNull(5) ? null : reader.GetString(5),
+                                OrganizationAddress = reader.IsDBNull(6) ? null : reader.GetString(6)
+                            };
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
         public bool Register(RE re)
         {
             using (var conn = DbHelper.GetConnection())
@@ -63,6 +95,38 @@ namespace Group16_iPERMITAPP.Models
                     }
                 }
             }
+        }
+
+        public List<RE> GetAll()
+        {
+            var list = new List<RE>();
+
+            using (var conn = DbHelper.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT REId, REPassword, contactPersonName, createdDate, REEmail, organizationName, organizationAddress FROM RE;";
+                using (var cmd = new SQLiteCommand(query, conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(new RE
+                            {
+                                REId = reader.IsDBNull(0) ? null : reader.GetString(0),
+                                REPassword = reader.IsDBNull(1) ? null : reader.GetString(1),
+                                ContactPersonName = reader.IsDBNull(2) ? null : reader.GetString(2),
+                                CreatedDate = reader.IsDBNull(3) ? null : reader.GetString(3),
+                                REEmail = reader.IsDBNull(4) ? null : reader.GetString(4),
+                                OrganizationName = reader.IsDBNull(5) ? null : reader.GetString(5),
+                                OrganizationAddress = reader.IsDBNull(6) ? null : reader.GetString(6)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return list;
         }
     }
 }
